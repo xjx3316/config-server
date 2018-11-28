23e30a5311aac1110a2c4c8c6bca6dccd0f7992e
xjx <xjx@qq.com>
2018-11-28 13:56:08 +0800

    xjx

--- src/main/java/com/didispace/ConfigServerApplication.java
+++ src/main/java/com/didispace/ConfigServerApplication.java
@@ -1,10 +1,10 @@
 package com.didispace;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
